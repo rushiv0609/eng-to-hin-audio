2c2554230d7b86420ff11dfbbe7871e5a7b196dc
rushiv0609 <rushiv0609@gmail.com>
2020-09-17 01:22:29 +0530

    Update run-first.sh

--- run-first.sh
+++ run-first.sh
@@ -5,3 +5,6 @@ mkdir hindi_audio
 wget https://github.com/mozilla/DeepSpeech/releases/download/v0.4.1/deepspeech-0.4.1-models.tar.gz
 tar -xvzf deepspeech-0.4.0-models.tar.gz
 mv models deepspeech_model
+sudo add-apt-repository ppa:jonathonf/ffmpeg-4
+sudo apt-get update
+sudo apt-get install ffmpeg
